3d78ddaa6cdd9401179ebf43b6304d5998593bb2
VelVelikov <webeltek12@gmail.com>
2025-06-07 11:39:06 +0200

    fix the code

--- CircularQueue.Tests/CircularQueue.cs
+++ CircularQueue.Tests/CircularQueue.cs
@@ -54,7 +54,7 @@ namespace CircularQueue.Tests
 
         public T Dequeue()
         {
-            if (this.Count == 200)
+            if (this.Count == 0)
                 throw new InvalidOperationException("The queue is empty!");
 
             T result = this.elements[this.StartIndex];
